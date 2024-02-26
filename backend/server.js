3e20d4822fc2e2fbf576a78be1ab4aa1acb92ea0
Anurag Ranjan <anurag.botmaster@outlook.com>
2024-02-26 15:01:58 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ main().catch((err) => console.log(err));
 
 async function main() {
     const app = express();
-    app.use(cors());
+    app.use(cors({
+        origin: '*', // Allow requests from any origin
+        methods: 'GET,PUT,POST,DELETE', // Allow specified HTTP methods
+        allowedHeaders: 'Content-Type,Authorization', // Allow specified headers
+      }));
     app.use(express.json());
     app.use("/api", routes);
 
